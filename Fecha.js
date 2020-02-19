b8fca7d068d71073d542ef448ada59321430ae00
martinmt-mx <mmojica0@ucol.mx>
2020-02-19 01:28:17 -0600

    Fecha

--- Fecha.js
+++ Fecha.js
@@ -1,19 +1,72 @@
 export default class Fecha{
     /**
      * 
-     * @param {number} año 
-     * @param {number} mes 
-     * @param {number} dia 
+     * @param {number} dia numero del dia del año de cita
+     * @param {number} mes dia del mes del año de cita
+     * @param {number} año año de cita
      */
     constructor(dia, mes, año){
         this.fecha = new Date (año, mes-1, dia)
+        this.diaSemana = ["Domingo", "Lunes", "Martes", "Miércoles", "Jueves", "Viernes", "Sábado"]
     }
     getAños(){
         let años = this.fecha.getFullYear() - new Date(Date.now()).getFullYear()
         return años
     }
     getMeses(){
-        let meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12)-new Date(Date.now).getMonth
+        let mes1 = new Date(Date.now()).getMonth()
+        let mes2 = this.fecha.getMonth()
+        let meses
+        if(mes1<mes2){
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12) + (mes2-mes1)
+        }
+        else if(mes1>mes2){
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12) - (mes1-mes2)
+        }
+        else{
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12)
+        }
         return meses
+    }
+    getSemanas(){
+        let mes1 = new Date(Date.now()).getMonth()
+        let mes2 = this.fecha.getMonth()
+        let meses
+        if(mes1<mes2){
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12) + (mes2-mes1)
+        }
+        else if(mes1>mes2){
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12) - (mes1-mes2)
+        }
+        else{
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12)
+        }
+        let semanas = meses * 4
+        return semanas
+    }
+    getDias(){
+        let mes1 = new Date(Date.now()).getMonth()
+        let mes2 = this.fecha.getMonth()
+        let meses
+        if(mes1<mes2){
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12) + (mes2-mes1)
+        }
+        else if(mes1>mes2){
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12) - (mes1-mes2)
+        }
+        else{
+            meses = ((this.fecha.getFullYear() - new Date(Date.now()).getFullYear())*12)
+        }
+        let semanas = meses * 4
+        let dias = semanas * 30
+        return dias
+    }
+    getFecha(){
+        let fechaCorta = `${this.fecha.getDate()}/${this.fecha.getMonth()+1}/${this.fecha.getFullYear()}`
+        return fechaCorta
+    }
+    getDiaFecha(){
+        let diaFecha = this.diaSemana[this.fecha.getDay()]
+        return diaFecha
     }
 }
--- Main.js
+++ Main.js
@@ -1,14 +1,35 @@
 import Fecha from "./Fecha.js"
 class Main{
-    probarFecha(){
-        let fecha1 = new Fecha(3,4,2022)
+    probarAño(){
+        let fecha1 = new Fecha(19,4,2024)
         console.log(fecha1.getAños())
     }
     probarMeses(){
-        let fecha1 = new Fecha(3,4,2022)
+        let fecha1 = new Fecha(19,2,2022)
         console.log(fecha1.getMeses())
+    }
+
+    probarSemanas(){
+        let fecha1 = new Fecha(19,2,2022)
+        console.log(fecha1.getSemanas())
+    }
+    probarDias(){
+        let fecha1 = new Fecha(19,2,2022)
+        console.log(fecha1.getDias())
+    }
+    probarFecha(){
+        let fecha1 = new Fecha(19,2,2022)
+        console.log(fecha1.getFecha())
+    }
+    probarDiaFecha(){
+        let fecha1 = new Fecha(19,2,2022)
+        console.log(fecha1.getDiaFecha())
     }
 }
 let app = new Main()
-app.probarFecha()
+app.probarAño()
 app.probarMeses()
+app.probarSemanas()
+app.probarDias()
+app.probarFecha()
+app.probarDiaFecha()
